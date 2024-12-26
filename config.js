49a54b17d275a46fb4f514104d9742b5122e9f4e
fahriasikamat <ajaalimm@gmail.com>
2024-12-26 14:18:27 +0800

    .com.azry

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7949998901:AAHT_sT5rNylMIZIMIx769VZQ56Bdkqx3pA", // Token bot Telegram
+  OWNER_ID: "6758060721", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
